ccb71a6b2babbf3617d590e3fc98820799bcbfb6
Jakob Farian Krarup <kr@rup.dk>
2022-11-14 09:22:50 +0100

    First push - model and website created

---
+++ BlogSharpCodeAlongWebsite/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogSharpCodeAlongWebsite.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "untitled";
+        public string Content { get; set; } = "No content";
+        public DateTime CreationTime { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ BlogSharpCodeAlongWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogSharpCodeAlongWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
